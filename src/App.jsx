12fd9b1bbead98a1db004fb552ad61f7c1ed9023
BasharatAli1 <basharatali0567@gmail.com>
2024-08-12 14:36:20 +0500

    add new code

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,18 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Counter from './components/counter'
+import ChildFunc from './components/child.func'
+import ChildClass from './components/child.class'
 
 function App() {
+	const [count, setCount] = useState(0);
+	const increment = () => {
+		setCount(count + 1);
+	}
   return (
     <>
-        <Counter></Counter>
+        <ChildFunc count={count}></ChildFunc>
+        <ChildClass count={count}></ChildClass>
     </>
   )
 }
