61f8b966059b007f4d3c13675e25ef269757582d
BasharatAli1 <basharatali0567@gmail.com>
2024-08-16 18:05:56 +0500

    login and routing of project

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,27 @@
-import { RouterProvider } from 'react-router-dom';
 import './App.css'
 import router from './components/router';
+import Login from './components/Login';
+import { useState } from 'react';
+import { RouterProvider } from 'react-router-dom';
   
 if (import.meta.hot) {
     import.meta.hot.dispose(() => router.dispose());
 }
 
-function App() {
-    return(
+const App = () => {
+    const [loginSuccess, setLoginSuccess] = useState(false);
+    const handleLoginResponse = (flag) => {
+        setLoginSuccess(flag);
+    }
+    return !loginSuccess ? (
+        <>
+            <Login handleLoginResponse={handleLoginResponse} />
+            <div>FLAG: {!loginSuccess ? 'False': 'True'}</div>
+        </>
+    ) : (
         <>
             <RouterProvider router={router} />;
+            <div>FLAG: {!loginSuccess ? 'False': 'True'}</div>
         </>
     )
   }
--- src/components/router.jsx
+++ src/components/router.jsx
@@ -3,12 +3,14 @@ import Home from "./Home";
 import About from "./About";
 import App from "../App";
 import Layout from "./Layout";
+import Login from "./Login";
 
 
 const routes = createRoutesFromElements(
     <Route path="/" element={<Layout />} >
         <Route index element={<Home />} />
         <Route path="about" element={<About />} />
+        {/* <Route path="login" element={<Login />} /> */}
     </Route>
 );
 const router = createBrowserRouter(routes);
