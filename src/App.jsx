cbe44334bff12a6609df769ebb7cad4bde0757ce
BasharatAli1 <basharatali0567@gmail.com>
2024-08-16 11:23:40 +0500

    remove old code

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,11 @@
-import { Fragment } from 'react'
 import './App.css'
-import Hero from './components/Hero'
-import NavBar from './components/NavBar'
 
 function App() {
 
   return (
-    <div className="container">
-      <NavBar />  
-      <Hero />  
-    </div>
+    <>
+
+    </>
   )
 }
 
