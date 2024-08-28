7644768da9ae6fad1a42beff8cae53ae8822950e
BasharatAli1 <basharatali0567@gmail.com>
2024-08-28 15:04:46 +0500

    add navbar icons

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,17 +1,28 @@
-import React from 'react'
+import React from 'react';
 import { useSelector } from 'react-redux';
-import { Link, Outlet } from 'react-router-dom'
+import { Link, Outlet } from 'react-router-dom';
+import '../assets/css/layout.css';
+import { ClinicIcon } from '../assets/icons/svg/clinicIcon.jsx'
+import { OrdersIcon } from '../assets/icons/svg/ordersIcon.jsx'
+import { PatientIcon } from '../assets/icons/svg/patientIcon.jsx'
 
 const Layout = () => {
     const isAuthenticated = useSelector(state => state.auth.isAuthenticated); // Adjust this line based on your auth state
     return (
         <>
         {isAuthenticated &&
-            <nav style={{ display: "flex", padding: "16px", justifyContent: "space-between", alignItems: "center" }}>
+            <nav className='nav-bar'>
                 <div>
-                <Link to="/order">Order</Link>&nbsp;&nbsp;
-                <Link to="/clinic">Clinic</Link>&nbsp;&nbsp;
-                <Link to="/patient">Patient</Link>&nbsp;&nbsp;
+                    <Link className='nav-bar-link' to="/order">
+                        <OrdersIcon /> Order
+                    </Link>&nbsp;&nbsp;
+                    <Link className='nav-bar-link' to="/clinic">
+                        <ClinicIcon /> Clinic
+                    </Link>&nbsp;&nbsp;
+                    <Link className='nav-bar-link' to="/patient">
+                        <PatientIcon /> Patient
+                    </Link>&nbsp;&nbsp;
+                    <button className='logout-button'>Logout</button>
                 </div>
             </nav>
         }
