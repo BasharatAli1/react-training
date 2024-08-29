71775b16feecc91744f481544e03691683f8efb8
BasharatAli1 <basharatali0567@gmail.com>
2024-08-29 11:01:11 +0500

    replace Link with NavLink to use active link

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { Link, Outlet, useNavigate } from 'react-router-dom';
+import { Link, NavLink, Outlet, useNavigate } from 'react-router-dom';
 import './layout.css';
 import { ClinicIcon } from '../../assets/icons/svg/clinicIcon.jsx'
 import { OrdersIcon } from '../../assets/icons/svg/ordersIcon.jsx'
@@ -16,32 +16,41 @@ const Layout = () => {
         dispatch(setAuth(false));
         navigate('/login');
     }
-    const isAuthenticated = useSelector(state => state.auth.isAuthenticated); // Adjust this line based on your auth state
+    const isAuthenticated = useSelector(state => state.auth.isAuthenticated);
+
+    const navLinkStyle = ({ isActive }) => ({
+        // display: 'flex',
+        // alignItems: 'center',
+        padding: '10px',
+        borderRadius: '6px',
+        border: isActive ? '2px solid #2196F3' : '2px solid transparent',  // Change border when active
+    });
+
     return (
         <>
-        {isAuthenticated && (
-            <nav className='nav-bar'>
-                <div className="nav-links">
-                    <Link className='nav-bar-link' to="/order">
-                        <OrdersIcon /> &nbsp; Order
-                    </Link>
-                    <Link className='nav-bar-link' to="/clinic">
-                        <ClinicIcon /> &nbsp; Clinic
-                    </Link>
-                    <Link className='nav-bar-link' to="/patient">
-                        <PatientIcon /> &nbsp; Patient
-                    </Link>
-                </div>
+            {isAuthenticated && (
+                <nav className="nav-bar">
+                    <div className="nav-links">
+                        <NavLink className="nav-bar-link" to="/order" style={navLinkStyle}>
+                            <OrdersIcon /> Order
+                        </NavLink>&nbsp;&nbsp;
+                        <NavLink className="nav-bar-link" to="/clinic" style={navLinkStyle}>
+                            <ClinicIcon /> Clinic
+                        </NavLink>&nbsp;&nbsp;
+                        <NavLink className="nav-bar-link" to="/patient" style={navLinkStyle}>
+                            <PatientIcon /> Patient
+                        </NavLink>&nbsp;&nbsp;
+                    </div>
                 <div className="nav-logout">
                     <button className='logout-button' onClick={handleLogout}>Logout</button>
                 </div>
-            </nav>
-        )}
-        <div style={{ textAlign: "center", padding: "16px" }}>
-            <Outlet />
-        </div>
+                </nav>
+            )}
+            <div style={{ textAlign: "center", padding: "16px" }}>
+                <Outlet />
+            </div>
         </>
-    )
-}
+    );
+};
 
 export default Layout;
