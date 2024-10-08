512c1c74bd0ab5cbbf91a7a634280fc8e360e690
BasharatAli1 <basharatali0567@gmail.com>
2024-10-08 16:15:12 +0500

    handle loged in user for hard refresh

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,10 +1,37 @@
 import { Navigate } from "react-router-dom";
-import { useSelector } from 'react-redux'; // Assuming you're storing the login state in Redux
+import { useDispatch, useSelector } from 'react-redux'; // Assuming you're storing the login state in Redux
+import { API } from "../axios";
+import { useEffect } from "react";
+import { setAuth } from '../slices/auth';
+
+const apiCall = async () => {
+    const result = await API.get('/profile/short');
+    console.log('api call');
+    return result;
+};
 
 const ProtectedRoute = ({ children }) => {
+    const dispatch = useDispatch();
     const isAuthenticated = useSelector(state => state.auth.isAuthenticated);
-    console.log('isAuthenticated', isAuthenticated);
-    
+
+    useEffect(() => {
+        if (!isAuthenticated) { // Conditionally run useEffect only if not authenticated
+            const fetchProfile = async () => {
+                console.log('useEffect Start');
+                const result = await apiCall();
+                console.log('Got Result', result);
+                if (result.data.status === "success") {
+                    dispatch(setAuth(true));
+                }
+                console.log('useEffect End');
+            };
+
+            fetchProfile();
+        }
+    }, [isAuthenticated, dispatch]); // Add isAuthenticated as a dependency
+
+    console.log('isAuthenticated ...', isAuthenticated);
+
     if (!isAuthenticated) {
         // Redirect to login page if user is not authenticated
         return <Navigate to="/login" />;
